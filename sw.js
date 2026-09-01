ebb8d061309b1f2f469ee4d1a844b3931935375e
nsooproomonien <nsooproomonien@gmail.com>
2026-09-01 19:55:33 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fleet-app-v14";
+const CACHE_NAME = "fleet-app-v15";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
